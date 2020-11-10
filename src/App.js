f9371b911d1f8b46bcb2717c6e5991b07a2387fb
JMustang <eecfredes@gmail.com>
2020-11-10 00:01:24 -0300

    created

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React from 'react'
 import Nav from './Nav'
 import About from './About'
 import Shop from './Shop'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
-      <About />
-      <Shop />
+      <Switch>
+      <Route path="/" exact component={ Home } />
+      <Route path="/about" component={ About } />
+      <Route path="/shop" component={ Shop } />
+      </Switch>
     </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+  )
+
 
 export default App;
