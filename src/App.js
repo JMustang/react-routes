d69cf8c65df5882b2618bc6f03ceed7bac96bde6
JMustang <eecfredes@gmail.com>
2020-11-09 22:09:54 -0300

    router study

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from './Nav'
+import About from './About'
+import Shop from './Shop'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <About />
+      <Shop />
+    </div>
+  );
+}
+
+export default App;
